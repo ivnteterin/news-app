ac9fea0da71b643e0aa3445f0a9cb74c50079438
ivnteterin <spy930712@gmail.com>
2023-04-20 16:33:26 +0300

    hello world console log test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true,
-        "jest": true,
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-console': 1,
+    indent: 'off',
+    quotes: [2, 'single'],
+    'linebreak-style': [2, 'unix'],
+    semi: [2, 'always'],
+  },
+};
