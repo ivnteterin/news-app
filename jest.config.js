03ba1138c91f1135bc2c746098cf49036eaf111a
ivnteterin <ivnteterin@gmail.com>
2024-01-22 00:57:55 +0200

    fix test add jest conf

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageDirectory: 'coverage',
   coverageReporters: ['json-summary', 'text', 'lcov'],
 }
