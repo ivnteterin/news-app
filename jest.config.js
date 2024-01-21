bbc3df4cf734e1832da13a0485ad61808ae6c845
ivnteterin <ivnteterin@gmail.com>
2024-01-22 01:28:37 +0200

    remove coverage

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  testEnvironment: 'node',
-  coverageDirectory: 'coverage',
-  coverageReporters: ['json-summary', 'text', 'lcov'],
-}
